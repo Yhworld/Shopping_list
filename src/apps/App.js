1eabf63a35db432268b04e083a51e30708325454
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-09-03 14:45:36 +0300

    Updated files

--- src/apps/App.js
+++ src/apps/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList";
+import Header from "./Header";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function handleDarkModeClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"App " + (isDarkMode ? "dark" : "light")}>
+      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+      <ShoppingList />
     </div>
   );
 }
---
+++ src/apps/ShoppingLIst.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import ItemForm from "./ItemForm";
+import Filter from "./Filter";
+import Item from "./Item";
+
+function ShoppingList() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+    .then((resp) => resp.json())
+    .then((items) => setItems(items));
+  }, []);
+
+  function handleDeleteItem(deleteditem) {
+    const updatedItems = items.filter((item) => item.id !== deleteditem.id);
+    setItems(updatedItems);
+  }
+
+  function handleUpdateItem(updatedItem) {
+    console.log("I ShoppingCart:", updatedItem);
+  }
+
+  function handleAddItem(newItem) {
+    setItems([...items,newItem]);
+  }
+
+  function handleCategoryChange(category) {
+    setSelectedCategory(category);
+  }
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+    return item.category === selectedCategory;
+  });
+
+  return (
+    <div className="ShoppingList">
+      <ItemForm onAddItem={handleAddItem} />
+      <Filter
+        category={selectedCategory}
+        onCategoryChange={handleCategoryChange}
+      />
+      <ul className="Items">
+        {itemsToDisplay.map((item) => (
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} onDeleteItem={handleDeleteItem} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default ShoppingList;
